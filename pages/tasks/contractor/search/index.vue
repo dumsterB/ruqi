b09fbd1553dec4fcb14cc48a65cc26833d114f89
itWelt <rassulovmurad@gmail.com>
2022-05-18 18:04:07 +0300

    [RUQI-316][story] file system architecture implementation

---
+++ pages/tasks/contractor/search/index.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+.rq-tasks__executor-search
+  mContentDisplayController
+  TasksFilter
+  ContentDisplayController
+  mTasksFilter
+  TasksList
+  mTasksList
+</template>
+
+<script>
+import ContentDisplayController from '@/components/pages/tasks/executor/desktop/ContentDisplayController';
+import mContentDisplayController from '@/components/pages/tasks/executor/mobile/mContentDisplayController';
+import TasksFilter from '@/components/pages/tasks/executor/desktop/TasksFilter';
+import mTasksFilter from '@/components/pages/tasks/executor/mobile/mTasksFilter';
+import TasksList from '@/components/pages/tasks/executor/desktop/TasksList';
+import mTasksList from '@/components/pages/tasks/executor/mobile/mTasksList';
+
+export default {
+  components: {
+    ContentDisplayController,
+    mContentDisplayController,
+    TasksFilter,
+    mTasksFilter,
+    TasksList,
+    mTasksList,
+  },
+  props: {},
+  computed: {},
+  watch: {},
+  methods: {
+    /* GETTERS */
+    /* SETTERS */
+    /* HANDLERS */
+    /* HELPERS */
+  },
+
+  data() {
+    return {}
+  },
+  created() {},
+  mounted() {},
+}
+</script>
+
+<style lang="scss">
+.rq-tasks__executor-search {
+
+}
+</style>
