d83a32f3e051a123782cb7945c85451601f1bc26
Dumster <abduvalievbehr.uz1@gmail.com>
2022-05-26 14:44:33 +0500

    add singup page for executor

---
+++ components/executor/auth/Auth.vue
@@ -0,0 +1,71 @@
+<template>
+<div class="auth">
+  <div class="form">
+    <img class="img" src="@/assets/img/rocket.svg" alt="">
+    <div class="info">
+      <h1>Стать исполнителем</h1>
+      <h4>Ruqi.ru – сервис быстрого поиска работы на любой промежуток времени. <br> Тут рыбный текст про преимущества сервиса руки</h4>
+    </div>
+    <div class="actions">
+      <v-btn dark elevation="0" class="btn-primary"><span class="btn-title">зарегистрироваться</span> </v-btn>
+      <v-btn  elevation="0" class="btn-secondary"> <span class="btn-title">Войти</span> </v-btn>
+      <p class="signin-text">Уже зарегистрировались? Нажимайте войти</p>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Auth"
+}
+</script>
+
+<style scoped>
+.auth{
+  justify-content: center;
+  display: flex;
+  margin-top: 20px;
+  padding-right: 20px;
+  padding-left: 20px;
+}
+.img{
+  height: 250px;
+  margin: auto;
+  justify-content: center;
+  display: flex;
+}
+.info{
+  text-align: center;
+}
+.actions{
+  margin-top: 15px;
+}
+.btn-primary{
+  background: #0082DE!important;
+  border-radius: 8px;
+  height: 45px!important;
+  width: 100%;
+}
+.btn-secondary{
+  background: white!important;
+  border-radius: 8px;
+  height: 45px!important;
+  width: 100%;
+  border: 1px solid lightgrey;
+  margin-top: 15px;
+}
+.btn-title{
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 125%;
+}
+.signin-text{
+  color: #263043;
+  font-size: 16px;
+  opacity: 0.4;
+  text-align: center;
+  margin-top: 15px;
+  cursor: pointer;
+}
+</style>
