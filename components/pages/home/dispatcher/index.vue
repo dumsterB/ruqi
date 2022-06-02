02955499f8e10e1009ef69307e0438f681f9bccf
itWelt <rassulovmurad@gmail.com>
2022-06-02 14:01:40 +0300

    [RUQI-477][task] completed

---
+++ components/pages/home/dispatcher/index.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+.rq-home-dispatcher dispatcher
+</template>
+
+<script>
+export default {
+  components: {},
+
+  props: {},
+  data: () => ({}),
+  computed: {},
+
+  watch: {},
+  methods: {
+    /* GETTERS */
+    /* SETTERS */
+    /* HANDLERS */
+    /* HELPERS */
+  },
+
+  created() { },
+  mounted() { },
+}
+</script>
+
+<style lang="scss">
+</style>
--- middleware/access_rights.js
+++ middleware/access_rights.js
@@ -1,22 +1,22 @@
 
-export default function access_rights ({ route, store, redirect}){
+export default function access_rights({ route, store, redirect }) {
+  let user = store.getters['user/user'];
+  let subroute = false;
 
-  let user = store.getters['user/user'],
-      subroute = false;
-
-  if (Object.keys(user).length != 0){
-    user.visibility_sections.forEach(function(item){
-      if(route.name && route.name.includes(item)){
+  if (Object.keys(user).length != 0) {
+    user.visibility_sections.forEach(function (item) {
+      if (route.name && route.name.includes(item)) {
         subroute = true;
       }
-    })
+    });
 
-    if(route.name != 'index'
+    if (
+      route.name != 'index'
       && route.name != 'signin'
       && route.name !== 'auth-signup-executor'
       && route.name !== 'auth-signup-executor-registration'
-      && !(user.visibility_sections.includes(route.name) || subroute)){
-        console.debug('Access rights');
+      && !(user.visibility_sections.includes(route.name) || subroute)
+    ) {
       redirect('/');
     }
   }
