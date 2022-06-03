40b6ce5c87cd5f7eac1a11613d0942fafb1a1f9f
itWelt <rassulovmurad@gmail.com>
2022-06-03 14:24:02 +0300

    [RUQI-454][task] added user bar

---
+++ components/UI/Avatar/index.vue
@@ -0,0 +1,64 @@
+<template lang="pug">
+v-avatar.ui-avatar(
+  :color="avatarBackground"
+  :height="height"
+  :width="width"
+)
+  v-icon.ui-avatar__stub(
+    :color="avatarLogoBackground"
+  ) mdi-camera
+</template>
+
+<script>
+export default {
+  components: {},
+
+  props: {
+    logo: {
+      type: String,
+      default: '',
+    },
+
+    theme: {
+      type: String,
+      default: '',
+    },
+  },
+  data: () => ({}),
+  computed: {
+    avatarBackground() {
+      switch (this.theme) {
+        case 'blue':
+          return '#349CE4';
+
+        default:
+          return '#D5DEE8';
+      }
+    },
+    avatarLogoBackground() {
+      switch (this.theme) {
+        case 'blue':
+          return '#2990D7';
+
+        default:
+          return '#FFFFFF';
+      }
+    },
+  },
+
+  watch: {},
+  methods: {
+    /* GETTERS */
+    /* SETTERS */
+    /* HANDLERS */
+    /* HELPERS */
+  },
+
+  created() { },
+  mounted() { },
+}
+</script>
+
+<style lang="scss">
+.ui-avatar {}
+</style>
