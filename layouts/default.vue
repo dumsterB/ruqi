d10b741bc8701920096ceec823ca86e00268c486
itWelt <rassulovmurad@gmail.com>
2022-06-07 09:50:24 +0300

    [RUQI-482][task] added init by platformDetection

--- layouts/default.vue
+++ layouts/default.vue
@@ -118,6 +118,7 @@ export default {
   },
 
   async created() {
+    this.$platformDetection.initPlatforms({ width: Number(window.outerWidth) });
     this.$platformDetection.onSubscribe();
     await this.$socket.setQuery({
       ...this.$socket.query,
